9590f19665a4fa66c1d19ac09e4f5db88ab1a46f
erickyenque <erickyeme9@gmail.com>
2018-08-24 21:20:15 -0500

    Creacion de Selector de Colores
    Creacion de Selector de Colores

---
+++ SC-OpenGL/src/sc/opengl/SCOpenGL.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sc.opengl;
+
+/**
+ *
+ * @author Ultimate
+ */
+public class SCOpenGL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SelectorColor SC = new SelectorColor();
+        SC.setLocation(400, 200);
+        SC.show();
+    }   
+    
+}
